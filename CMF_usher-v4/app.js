a1222dc264a3d8c0af97cf57354599fb2f6d9183
Daniel O'Brien <wheresmypen@yahoo.com>
2014-12-08 17:25:45 -0700

    first v4 commit

--- CMF_usher-v4/app.js
+++ CMF_usher-v4/app.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'jade');
 
 app.use(favicon());
@@ -19,7 +19,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
