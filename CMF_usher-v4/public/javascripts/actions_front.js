539c06a141015452b4a4d6336363e26c4ee1958b
Daniel O'Brien <wheresmypen@yahoo.com>
2014-12-10 11:22:07 -0700

    basic navigation

--- CMF_usher-v4/public/javascripts/actions_front.js
+++ CMF_usher-v4/public/javascripts/actions_front.js
@@ -1,7 +1,24 @@
+// This log-out function is disabled, but ought to toggle to global id object
+// It will be available on every page in the basic layout
+
 $('#exitUser').on('click', function() {
     console.log('button works');
 });
 
-$('#showSite').on('click', function(){
+// HERE are the navigation buttons within the body
+
+$('#optionsProfile').on('click', function() {
+    window.location.href = 'http://localhost:3000/optionsProfile';
+});
+
+$('#fullSchedule').on('click', function() {
+    window.location.href = 'http://localhost:3000/fullSchedule';
+});
+
+$('#startPage').on('click', function() {
+    window.location.href = 'http://localhost:3000/startPage';
+});
+
+$('#siteMap').on('click', function(){
    window.location.href = 'http://localhost:3000/siteMap';
 });
